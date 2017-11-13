a713c96bb3b314e10b7e1855a203002beae99d3e
Tucker Boniface <tucker@boniface.tech>
2017-11-13 15:11:46 -0800

    :thonk:

--- main.c
+++ main.c
@@ -54,14 +54,10 @@ int get_lines(char* file) {
 }
 
 char** split(const char* str, const char* s) {
-    //char* str = malloc(sizeof(str));
-    //char* s = malloc(sizeof(s));
-    //memcpy(&str, &str_cp, strlen(str_cp)+1);
-    //memcpy(&s, &s_cp, strlen(s_cp)+1);
-    char* no_const = malloc(sizeof(str)+10);
+    char* no_const = malloc(sizeof(str)+1);
     memcpy(no_const, str, strlen(str)+1);
     char* token;
-    char** tokens = malloc(sizeof(str));
+    char** tokens = malloc(sizeof(str)+1);
     int counter = 0;
     /* get the first token */
     token = strtok(no_const, s);
@@ -95,9 +91,8 @@ NodeList get_nodes(char* file, bool verbose) {
     int lines = get_lines(file);
     char line[LINE_MAX];
     Node NullNode = make_nullnode();
-    Node* node_tmp = malloc(sizeof(Node) * 10);
+    Node* node_tmp = malloc(sizeof(Node) * 30);
     NodeList nodes = {node_tmp, 0};
-    nodes.count = 0;
     int counter = 0;
 
     for (int i = 0; i<lines; i++) {
@@ -113,18 +108,17 @@ NodeList get_nodes(char* file, bool verbose) {
             continue;
         NearNode* neighbors = malloc(sizeof(NearNode) * 20);
         int neighbors_counter = 0;
-        //printf("line is %s", line);
         char** neighbors_in = split(split(line, ":")[1], ",");
-        //printf("line is now %s\n", line);
         while (neighbors_in[neighbors_counter] != NULL) {
-            neighbors[neighbors_counter].name = split(neighbors_in[neighbors_counter], "=")[0];
-            neighbors[neighbors_counter].weight = atoi(split(neighbors_in[neighbors_counter], "=")[1]);
+            //neighbors[neighbors_counter].name = split(neighbors_in[neighbors_counter], "=")[0];
+            //neighbors[neighbors_counter].weight = atoi(split(neighbors_in[neighbors_counter], "=")[1]);
             neighbors_counter++;
         }
         nodes.nodes[counter].name = split(line, ":")[0];
         printf("node name is %s", nodes.nodes[counter].name);
-        nodes.nodes[counter].length = neighbors_counter;
-        nodes.nodes[counter].neighbors = neighbors;
+        //nodes.nodes[counter].length = neighbors_counter;
+        //nodes.nodes[counter].neighbors = neighbors;
+
         counter++;
         if (verbose) {
             printf("%s", line);
